9e72b9e7bfaf8e0f1481e568b53c5a78d08b0bc1
Eunju Yang <ej.yang@samsung.com>
2026-03-18 11:59:20 +0900

    [CausalLM] fix mmap read in tie-word-embedding
    - This commit fixes the bugs in tie-word-embedding
    - Tie-word-embedding didn't support read with mmap before.
    - This commit adds read with mmap and remove enforced flag of mmap
      disablement when enable-transformer is enabled
    
    Signed-off-by: Eunju Yang <ej.yang@samsung.com>

--- Applications/CausalLM/layers/tie_word_embedding.cpp
+++ Applications/CausalLM/layers/tie_word_embedding.cpp
@@ -359,6 +359,30 @@ void TieWordEmbedding::read(
   }
 }
 
+void TieWordEmbedding::read(
+  nntrainer::ReadSource src, nntrainer::RunLayerContext &context, bool opt_var,
+  ml::train::ExecutionMode mode, bool trainable,
+  nntrainer::TensorDim::DataType definedWeightDataType, bool fsu,
+  size_t start_offset, bool read_from_offset) {
+
+  // Only read when mode is embedding
+  if (mode_ == mode::embedding) {
+    std::cout << "embedding read is called" << std::endl;
+    for (unsigned int i = 0; i < context.getNumWeights(); ++i) {
+      /// @note shared weights are only be read at the first acecss
+      if (context.isGradientFirstAccess(i)) {
+        context.getWeight(i).read(src);
+        if (context.isMixedPrecision(i) && trainable &&
+            !context.getWeightFP32(i).empty()) {
+          context.getWeightFP32(i).copyData(context.getWeight(i));
+        }
+      }
+    }
+  } else {
+    std::cout << "lmhead read is called" << std::endl;
+  }
+}
+
 void TieWordEmbedding::save(
   std::ofstream &file, nntrainer::RunLayerContext &run_context, bool opt_var,
   ml::train::ExecutionMode mode, bool trainable,
