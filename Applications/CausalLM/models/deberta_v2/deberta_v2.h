b08c4d7a4473d95350510a1cf120472a1e3bf0af
SeungHui Lee <shsh1004.lee@samsung.com>
2026-03-25 15:54:49 +0900

    [Fix] apply sentence_transformer rather than embedding
    This commit applies sentence_transformer rather than embedding.
    
    **Self evaluation:**
    Build test: [x]Passed [ ]Failed [ ]Skipped
    Run test: [x]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: SeungHui Lee <shsh1004.lee@samsung.com>

--- Applications/CausalLM/models/deberta_v2/deberta_v2.h
+++ Applications/CausalLM/models/deberta_v2/deberta_v2.h
@@ -2,19 +2,19 @@
 /**
  * Copyright (C) 2026 Seunghui Lee <shsh1004.lee@samsung.com>
  *
- * @file   deberta_v2.h
+ * @file   debertav2.h
  * @date   14 January 2026
  * @see    https://github.com/nntrainer/nntrainer
  * @author Seunghui Lee <shsh1004.lee@samsung.com>
  * @bug    No known bugs except for NYI items
- * @brief  Please refer to the following code :
- *  https://github.com/huggingface/transformers/blob/5c1c72b/src/transformers/models/deberta_v2/modeling_deberta_v2.py
+ * @note  Please refer to the following code :
+ * https://github.com/huggingface/transformers/blob/5c1c72b/src/transformers/models/deberta/modeling_deberta.py
  */
 
 #ifndef __DEBERTA_V2_H__
 #define __DEBERTA_V2_H__
 
-#include "../embedding.h"
+#include <sentence_transformer.h>
 #include <transformer.h>
 
 namespace causallm {
@@ -22,14 +22,14 @@ namespace causallm {
 /**
  * @brief DebertaV2 class
  */
-class DebertaV2 : public Embedding {
+class DebertaV2 : public SentenceTransformer {
 
 public:
   static constexpr const char *architectures = "DebertaV2";
 
   DebertaV2(json &cfg, json &generation_cfg, json &nntr_cfg) :
     Transformer(cfg, generation_cfg, nntr_cfg, ModelType::EMBEDDING),
-    Embedding(cfg, generation_cfg, nntr_cfg) {
+    SentenceTransformer(cfg, generation_cfg, nntr_cfg) {
     setupParameters(cfg, generation_cfg, nntr_cfg);
   }
 
@@ -45,9 +45,9 @@ protected:
                                               std::string input_name,
                                               std::string rel_embeddings_name);
 
-  std::vector<LayerHandle>
-  createDebertaV2Attention(const int layer_id, std::string input_name,
-                           std::string rel_embeddings_name);
+  std::vector<LayerHandle> createDebertaV2Attention(const int layer_id,
+                                                    std::string input_name,
+                                                    std::string rel_embeddings_name);
 
   /**
    * @brief Setup the parameters for the Deberta V2 model
@@ -59,12 +59,6 @@ protected:
    * @brief register CustomLayers
    */
   void registerCustomLayers() override;
-
-  /**
-   * @brief Encode the prompt and return the embedding
-   */
-  std::vector<float *> encode(const WSTR prompt, const WSTR system_prompt,
-                              const WSTR tail_prompt) override;
 
   int MAX_RELATIVE_POSITIONS;
   bool c2p;
