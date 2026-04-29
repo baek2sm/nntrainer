405cdedc9a7581aa31c8be19bae3bb3d0206e124
SeungHui Lee <shsh1004.lee@samsung.com>
2026-04-29 16:47:58 +0900

    [CausalLM] Apply bert_transformer to android build
    This commit applies bert_transformer to android build.
    
    **Self evaluation:**
    Build test: [x]Passed [ ]Failed [ ]Skipped
    Run test: [x]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: SeungHui Lee <shsh1004.lee@samsung.com>

--- Applications/CausalLM/jni/Android.mk
+++ Applications/CausalLM/jni/Android.mk
@@ -26,7 +26,7 @@ CAUSALLM_COMMON_INCLUDES := \
     $(LOCAL_PATH)/../models/qwen3_slim_moe \
     $(LOCAL_PATH)/../models/qwen3_cached_slim_moe \
     $(LOCAL_PATH)/../models/gemma3 \
-    $(LOCAL_PATH)/../models/tiny_bert \
+    $(LOCAL_PATH)/../models/bert \
 
 # Prebuilt nntrainer libraries
 include $(CLEAR_VARS)
@@ -92,7 +92,8 @@ LOCAL_SRC_FILES := \
     ../models/gemma3/gemma3_causallm.cpp \
     ../models/gemma3/embedding_gemma.cpp \
     ../models/gemma3/function.cpp \
-    ../models/tiny_bert/multilingual_tinybert_16mb.cpp \
+    ../models/bert/bert_transformer.cpp \
+    ../models/bert/multilingual_tinybert_16mb.cpp \
 
 LOCAL_SHARED_LIBRARIES := nntrainer ccapi-nntrainer
 LOCAL_STATIC_LIBRARIES := tokenizers_c
@@ -217,6 +218,8 @@ LOCAL_SRC_FILES := ../quantize.cpp \
     ../models/gpt_oss_cached_slim/gpt_oss_moe_layer_cached.cpp \
     ../models/gemma3/gemma3_causallm.cpp \
     ../models/gemma3/embedding_gemma.cpp \
+    ../models/bert/bert_transformer.cpp \
+    ../models/bert/multilingual_tinybert_16mb.cpp \
 
 LOCAL_SHARED_LIBRARIES := nntrainer ccapi-nntrainer
 LOCAL_STATIC_LIBRARIES := tokenizers_c
@@ -233,5 +236,6 @@ LOCAL_C_INCLUDES += $(NNTRAINER_INCLUDES) \
     $(LOCAL_PATH)/../models/qwen3_slim_moe \
     $(LOCAL_PATH)/../models/qwen3_cached_slim_moe \
     $(LOCAL_PATH)/../models/gemma3 \
+    $(LOCAL_PATH)/../models/bert \
 
 include $(BUILD_EXECUTABLE)
