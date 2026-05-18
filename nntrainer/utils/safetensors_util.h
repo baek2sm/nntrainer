d33296ddb779fd97b0738986cec2ef9ea895bf40
SeungHui Lee <shsh1004.lee@samsung.com>
2026-05-18 14:51:43 +0900

    utils: add safetensors utility functions
    Add safetensors_util.cpp/h for safetensors format support.
    Add ModelFormat type to API headers for format specification.
    Update meson.build accordingly.
    
    Signed-off-by: SeungHui Lee <shsh1004.lee@samsung.com>

---
+++ nntrainer/utils/safetensors_util.h
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2026 Seunghui Lee <shsh1004.lee@samsung.com>
+ *
+ * @file   safetensors_util.h
+ * @date   18 May 2026
+ * @brief  Minimal helpers for the safetensors on-disk format.
+ */
+
+#ifndef __SAFETENSORS_UTIL_H__
+#define __SAFETENSORS_UTIL_H__
+#ifdef __cplusplus
+
+#include <string>
+#include <tensor_dim.h>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+namespace nntrainer::safetensors {
+
+struct TensorEntry {
+  std::string name;
+  std::string dtype;
+  std::vector<size_t> shape;
+  size_t offset_start;
+  size_t offset_end;
+};
+
+const char *dtypeToString(ml::train::TensorDim::DataType dtype);
+
+std::string buildHeader(const std::vector<TensorEntry> &entries);
+
+std::unordered_map<std::string, std::pair<size_t, size_t>>
+parseHeader(const std::string &json);
+
+} // namespace nntrainer::safetensors
+
+#endif /* __cplusplus */
+#endif /* __SAFETENSORS_UTIL_H__ */
